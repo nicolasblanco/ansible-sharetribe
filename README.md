5a5c8795470210acb765904683652ee40296aa25
slainer68 <slainer68@gmail.com>
2014-08-11 15:15:26 +0200

    Use unicorn

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # My Ansible scripts to deploy Sharetribe
 
-**Bastardised from https://github.com/dodecaphonic/ansible-rails-app**
-
 It installs:
 
 - Ruby 2.1
 - MySQL
 - nginx
-- Puma (jungle)
+- Unicorn
 
-1. Change the app name and deploy directory in <code>vars/defaults.yml</code>.
+1. Copy <code>vars/defaults.yml.example</code> to <code>vars/defaults.yml</code> and change the vars.
 2. Rename `hosts.example` to `hosts` and change it to your hosts.
 
 To run:
 
-    $ ansible-playbook -i hosts ruby-webapp.yml -t ruby,deploy,mysql,nginx,ufw,import_data
-    $ <deploy your app> (BRANCH=puma HOST=XXX.XX.XX.XX bin/cap production deploy --trace)
-    $ ansible-playbook -i hosts ruby-webapp.yml -t puma
+    $ ansible-playbook -i hosts ruby-webapp.yml -t swap,ruby,deploy,mysql,nginx,ufw,import_data
+    $ <deploy your app> (HOST=XXX.XX.XX.XX bin/cap production deploy --trace)
+    $ ansible-playbook -i hosts ruby-webapp.yml -t unicorn
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,56 +1,56 @@
 set :application, 'sharetribe'
-set :repo_url, 'git://github.com/nicolasblanco/sharetribe'
+set :repo_url, 'git://github.com/sharetribe/sharetribe'
 current_branch = (`git branch`.match(/\* (\S+)\s/m) || [])[1]
 set :branch, ENV['BRANCH'] || current_branch || "master" # you can use the 'branch' parameter on deployment to specify the branch you wish to deploy
 
 set :deploy_to, '/data/sharetribe'
 set :log_level, :info
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{tmp/sockets log config/puma public/sharetribe}
+set :linked_dirs, %w{tmp/sockets log config/unicorn public/sharetribe}
 set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
 
 # These puma settings are only here because capistrano-puma is borked.
 # See issue #4.
-set :puma_roles, :app
-set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
-set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
-set :puma_state, fetch(:sockets_path).join('puma.state')
-set :puma_log, -> { shared_path.join("log/puma-#{fetch(:stage )}.log") }
-set :puma_flags, nil
+#set :puma_roles, :app
+#set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
+#set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
+#set :puma_state, fetch(:sockets_path).join('puma.state')
+#set :puma_log, -> { shared_path.join("log/puma-#{fetch(:stage )}.log") }
+#set :puma_flags, nil
 
 set :bundle_flags, '--deployment'
 
 namespace :deploy do
   task :restart do
-    invoke 'puma:restart'
+    #invoke 'puma:restart'
   end
 end
-
-namespace :puma do
-  desc "Restart puma instance for this application"
-  task :restart do
-    on roles fetch(:puma_roles) do
-      within release_path do
-        execute :bundle, "exec pumactl -S #{fetch(:puma_state)} restart"
-      end
-    end
-  end
-
-  desc "Show status of puma for this application"
-  task :status do
-    on roles fetch(:puma_roles) do
-      within release_path do
-        execute :bundle, "exec pumactl -S #{fetch(:puma_state)} stats"
-      end
-    end
-  end
-
-  desc "Show status of puma for all applications"
-  task :overview do
-    on roles fetch(:puma_roles) do
-      within release_path do
-        execute :bundle, "exec puma status"
-      end
-    end
-  end
-end
+#
+# namespace :puma do
+#   desc "Restart puma instance for this application"
+#   task :restart do
+#     on roles fetch(:puma_roles) do
+#       within release_path do
+#         execute :bundle, "exec pumactl -S #{fetch(:puma_state)} restart"
+#       end
+#     end
+#   end
+#
+#   desc "Show status of puma for this application"
+#   task :status do
+#     on roles fetch(:puma_roles) do
+#       within release_path do
+#         execute :bundle, "exec pumactl -S #{fetch(:puma_state)} stats"
+#       end
+#     end
+#   end
+#
+#   desc "Show status of puma for all applications"
+#   task :overview do
+#     on roles fetch(:puma_roles) do
+#       within release_path do
+#         execute :bundle, "exec puma status"
+#       end
+#     end
+#   end
+# end
